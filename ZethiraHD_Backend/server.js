114770edd33473adfa8b3aeefd0f9acf219673df
RichardPinewood <ricardopintopinheiro2003@gmail.com>
2025-02-22 14:37:08 +0000

    comback

--- ZethiraHD_Backend/server.js
+++ ZethiraHD_Backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); 
+require("dotenv").config({ path: "./.env" })
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -10,10 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
+
 console.log("Mongo URI from .env:", MONGO_URI); 
 
 if (!MONGO_URI) {
-    console.error("ERROR: MONGO_URI is not defined. Check the .env file or Railway variables.");
+    console.error(" MONGO_URI is not defined. Check the .env file.");
     process.exit(1);
 }
 
@@ -21,15 +22,9 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-    process.exit(1);
-  });
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.static(path.join(__dirname, "../frontend/build")));
@@ -40,5 +35,4 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../ZethiraHD/home.html"));
 });
 
-// Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
